0a340ad39256621a0d1c91657619b78a9356498a
prasannan-robots <prasannanatarajan3595@gmail.com>
2021-05-16 19:22:58 +0530

    pointer flip

--- arm_vision/opencv_checks/pointer.py
+++ arm_vision/opencv_checks/pointer.py
@@ -5,6 +5,7 @@ cap = cv2.VideoCapture(0)
 
 while(True):
     ret, frame = cap.read()
+    frame = cv2.flip(frame,1)
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
